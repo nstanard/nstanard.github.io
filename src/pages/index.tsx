22aacac7d0eda41b445ff21fcad40253cd248025
Neal Stanard <nstanard@gmail.com>
2025-04-03 18:20:25 -0500

    change max width on landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,7 +38,7 @@ const IndexPage = () => {
 
       <div className="py-24 sm:py-32">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <div className="mx-auto max-w-2xl">
+          <div className="mx-auto max-w-4xl">
             <h2 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-dark-text sm:text-4xl">
               About Me
             </h2>
