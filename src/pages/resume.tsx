02d78da5c4006cee784a42fe70fb5b651103253d
Neal Stanard <nstanard@gmail.com>
2025-04-04 16:13:43 -0500

    update resume

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -41,7 +41,7 @@ const ResumePage = () => {
                 Resume
               </h2>
               <a
-                href="/Neal Stanard - Resume.pdf"
+                href="/Neal Stanard - Software Engineer.pdf"
                 className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-primary-600 hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
                 target="_blank"
                 rel="noopener noreferrer"
